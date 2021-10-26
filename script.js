40914154f391343691f7f7879556b276eb3b9af1
champwow <sukawit1909@gmail.com>
2021-10-26 22:54:19 +0700

    update ขยะเลื่อนได้

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.body.onscroll = function() {
     var condi1Top = condi1.offsetTop;
     var condi2Top = condi2.offsetTop;
     var condi3Top = condi3.offsetTop;
-    var condi4Top = condi4.offsetTop;
+
     if (scrollY + window.innerHeight - (condi1.offsetHeight) >= condi1Top && scrollY < condi1Top) {
         document.querySelector(".big-receipt").dataset.scene = 1
     } else if (scrollY + window.innerHeight - (condi2.offsetHeight) >= condi2Top && scrollY < condi2Top) {
@@ -20,15 +20,32 @@ document.body.onscroll = function() {
     } else if (scrollY + window.innerHeight - (condi4.offsetHeight)) {
         document.querySelector(".big-receipt").dataset.scene = 4
     }
+    var bagcondi1 = document.getElementById("bagcondi-1");
+    var bagcondi2 = document.getElementById("bagcondi-2");
+    var bagcondi1Top = bagcondi1.offsetTop;
+    var bagcondi2Top = bagcondi2.offsetTop;
+    console.log(bagcondi1Top)
+    if (scrollY + window.innerHeight - (bagcondi1.offsetHeight) >= bagcondi1Top) {
+        document.querySelector(".bagforfood").dataset.scene = 1
+        console.log("่jim")
+    } else {
+        document.querySelector(".bagforfood").dataset.scene = 0
+    }
+    if (scrollY + window.innerHeight - (bagcondi2.offsetHeight) >= bagcondi2Top) {
+        document.querySelector(".bigbag").dataset.scene = 1
+        console.log("่jim")
+    } else {
+        document.querySelector(".bigbag").dataset.scene = 0
+    }
 }
 
-function pop(){
+function pop() {
     var modal = document.getElementById("modal1");
     modal.style.display = "block";
     console.log(1);
 };
 
-function close_mod(){
+function close_mod() {
     console.log(1);
     var modal = document.getElementById("modal1");
     modal.style.display = "none";
@@ -37,6 +54,6 @@ function close_mod(){
 window.onclick = function(event) {
     var modal = document.getElementById("modal1");
     if (event.target == modal) {
-      modal.style.display = "none";
+        modal.style.display = "none";
     }
-}; 
+};
