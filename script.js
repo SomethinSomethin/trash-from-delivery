5f88447f00da89f860f091c1ecd527747511a8cd
champwow <sukawit1909@gmail.com>
2021-11-08 23:02:53 +0700

    เก่งเจ๋ง

--- script.js
+++ script.js
@@ -20,57 +20,13 @@ document.body.onscroll = function() {
     } else if (scrollY + window.innerHeight - (condi4.offsetHeight)) {
         document.querySelector(".big-receipt").dataset.scene = 4
     }
-    var bagcondi1 = document.getElementById("bagcondi-1");
+
     var bagcondi2 = document.getElementById("bagcondi-2");
-    var bagcondi1Top = bagcondi1.offsetTop;
     var bagcondi2Top = bagcondi2.offsetTop;
-    console.log(bagcondi1Top)
-    if (scrollY + window.innerHeight - (bagcondi1.offsetHeight) >= bagcondi1Top) {
-        document.querySelector(".bagforfood").dataset.scene = 1
-        console.log("่jim")
-    } else {
-        document.querySelector(".bagforfood").dataset.scene = 0
-    }
     if (scrollY + window.innerHeight - (bagcondi2.offsetHeight) >= bagcondi2Top) {
-        document.querySelector(".bigbag").dataset.scene = 1
+        document.querySelector("#bagglass").dataset.scene = 1
         console.log("่jim")
     } else {
-        document.querySelector(".bigbag").dataset.scene = 0
+        document.querySelector("#bagglass").dataset.scene = 0
     }
 }
-
-function pop() {
-    var modal = document.getElementById("modal1");
-    modal.style.display = "block";
-    console.log(1);
-};
-
-function close_mod() {
-    console.log(1);
-    var modal = document.getElementById("modal1");
-    modal.style.display = "none";
-};
-
-window.onclick = function(event) {
-    var modal = document.getElementById("modal1");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-function show_Detlekapu() {
-    document.querySelector(".bigbag").dataset.show = "kapu";
-
-};
-
-function show_Detleyentafo() {
-    document.querySelector(".bigbag").dataset.show = "yentafo";
-};
-
-function show_Detlericechicken() {
-    document.querySelector(".bigbag").dataset.show = "ricechicken";
-};
-
-function back() {
-    document.querySelector(".bigbag").dataset.show = "start";
-};
