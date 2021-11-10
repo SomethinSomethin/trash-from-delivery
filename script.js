6790294444a515fcca18d5c68f8af1dd46704c5f
champwow <sukawit1909@gmail.com>
2021-11-11 00:28:18 +0700

    ssds

--- script.js
+++ script.js
@@ -29,6 +29,49 @@ document.body.onscroll = function() {
     } else {
         document.querySelector("#bagglass").dataset.scene = 0
     }
+
+    var condip1 = document.getElementById("condip_1");
+    var condip2 = document.getElementById("condip-2");
+    var condip3 = document.getElementById("condip-3");
+    var condip4 = document.getElementById("condip-4");
+    var condip5 = document.getElementById("condip-5");
+    var condip6 = document.getElementById("condip-6");
+    var condip7 = document.getElementById("condip-7");
+    var condip8 = document.getElementById("condip-8");
+
+
+    var condpi1Top = condip1.offsetTop;
+    var condip2Top = condip2.offsetTop;
+    var condip3Top = condip3.offsetTop;
+    var condip4Top = condip4.offsetTop;
+    var condpi5Top = condip5.offsetTop;
+    var condip6Top = condip6.offsetTop;
+    var condip7Top = condip7.offsetTop;
+    var condip8Top = condip8.offsetTop;
+    var Y = window.getComputedStyle(condip_1, null).getPropertyValue('top');
+    console.log(Y);
+
+    if (scrollY + window.innerHeight - (condip1.offsetHeight) >= condpi1Top && scrollY < condpi1Top) {
+        document.querySelector(".psall").dataset.scene = 0
+    } else if (scrollY + window.innerHeight - (condip2.offsetHeight) >= condip2Top && scrollY < condip2Top) {
+        document.querySelector(".psall").dataset.scene = 1
+    } else if (scrollY + window.innerHeight - (condip3.offsetHeight) >= condip3Top && scrollY < condip3Top) {
+        document.querySelector(".psall").dataset.scene = 2
+    } else if (scrollY + window.innerHeight - (condip4.offsetHeight) >= condip4Top && scrollY < condip4Top) {
+        document.querySelector(".psall").dataset.scene = 3
+    } else if (scrollY + window.innerHeight - (condip5.offsetHeight) >= condpi5Top && scrollY < condpi5Top) {
+        document.querySelector(".psall").dataset.scene = 4
+    } else if (scrollY + window.innerHeight - (condip6.offsetHeight) >= condip6Top && scrollY < condip6Top) {
+        document.querySelector(".psall").dataset.scene = 5
+    } else if (scrollY + window.innerHeight - (condip7.offsetHeight) >= condip7Top && scrollY < condip7Top) {
+        document.querySelector(".psall").dataset.scene = 6
+    } else if (scrollY + window.innerHeight - (condip8.offsetHeight) >= condip8Top && scrollY < condip8Top) {
+        document.querySelector(".psall").dataset.scene = 7
+    }
+
+
+
+
 }
 
 function show_Detleyentafo() {
